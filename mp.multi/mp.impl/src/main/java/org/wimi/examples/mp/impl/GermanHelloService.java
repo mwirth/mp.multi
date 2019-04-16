3307e9c6d42067bc82dc74692f6a001a5ee94a6a
Michael Wirth <mwirth@meyle-mueller.de>
2019-04-16 23:15:37 +0200

    added injecting special implementation with help of annotations

--- mp.multi/mp.impl/src/main/java/org/wimi/examples/mp/impl/GermanHelloService.java
+++ mp.multi/mp.impl/src/main/java/org/wimi/examples/mp/impl/GermanHelloService.java
@@ -3,15 +3,15 @@ package org.wimi.examples.mp.impl;
 import javax.enterprise.context.ApplicationScoped;
 
 import org.wimi.examples.mp.interfaces.HelloService;
-import org.wimi.examples.mp.interfaces.SayHelloEnglish;
+import org.wimi.examples.mp.interfaces.SayHelloGerman;
 
 @ApplicationScoped
-@SayHelloEnglish
-public class EnglishHelloService implements HelloService {
+@SayHelloGerman
+public class GermanHelloService implements HelloService {
 
 	@Override
 	public String sayHelloTo(String name) {
-		return ">" + getClass().getName() + "< Hello " + name;
+		return ">" + getClass().getName() + "< Hallo " + name;
 	}
 
 }
--- mp.multi/mp.services/src/main/java/org/wimi/examples/mp/services/Hello.java
+++ mp.multi/mp.services/src/main/java/org/wimi/examples/mp/services/Hello.java
@@ -7,18 +7,58 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 import org.wimi.examples.mp.interfaces.HelloService;
+import org.wimi.examples.mp.interfaces.SayHelloEnglish;
+import org.wimi.examples.mp.interfaces.SayHelloGerman;
 
 @Path("hello")
 @RequestScoped
 public class Hello {
 
+	// @Inject
+	// private HelloService helloService;
+
 	@Inject
-	private HelloService helloService;
+	@SayHelloGerman
+	private HelloService germanHelloService;
+
+	@Inject
+	@SayHelloEnglish
+	private HelloService englishHelloService;
 
 	@GET
 	@Path("{name}")
 	public String hello(@PathParam("name") String name) {
-		return helloService.sayHelloTo(name) + "!!";
+		return "simple hello";
+	}
+
+	// @GET
+	// @Path("sayHello/{name}")
+	// public String sayHello(@PathParam("name") String name) {
+	// return helloService.sayHelloTo(name);
+	// }
+
+	/**
+	 * http://localhost:8080/mp.services/data/hello/sayHelloGerman/{name}
+	 *
+	 * @param name
+	 * @return
+	 */
+	@GET
+	@Path("sayHelloGerman/{name}")
+	public String sayHelloGerman(@PathParam("name") String name) {
+		return germanHelloService.sayHelloTo(name);
+	}
+
+	/**
+	 * http://localhost:8080/mp.services/data/hello/sayHelloEnglish/{name}
+	 *
+	 * @param name
+	 * @return
+	 */
+	@GET
+	@Path("sayHelloEnglish/{name}")
+	public String sayHelloEnglish(@PathParam("name") String name) {
+		return englishHelloService.sayHelloTo(name);
 	}
 
 }
